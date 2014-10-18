e465521c0e68eaae1aa522904db2366a788de91b
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-10-18 19:55:54 +0400

    fix github auth

---
+++ spec/controllers/web/oauths_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Web::OauthsController, :type => :controller do
+
+  describe "GET oauth" do
+    it "returns http success" do
+      pending
+    end
+  end
+
+  describe "GET callback" do
+    it "returns http success" do
+      pending
+    end
+  end
+
+end
