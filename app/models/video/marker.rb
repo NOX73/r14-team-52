fa07e7f99881fcaf5bf607614d3238cd591e6d6d
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-10-18 16:04:50 +0400

    add more tests

--- app/models/video/marker.rb
+++ app/models/video/marker.rb
@@ -1,7 +1,7 @@
 class Video::Marker < ActiveRecord::Base
   belongs_to :video
 
-  enum type_of_marker: [:info, :link]
+  # enum type_of_marker: {info: 0, link: 1}
 
   validates :name, presence: true
   validates :start_at, presence: true
--- spec/controllers/api/videos/markers_controller_spec.rb
+++ spec/controllers/api/videos/markers_controller_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::Videos::MarkersController, :type => :controller do
 
-  let(:valid_attributes) {
-    attributes_for(:video_marker)
-  }
+  let(:valid_attributes) { attributes_for(:video_marker) }
 
   let(:invalid_attributes) {
-    {name: 'Test', type: 4, x: 0, y: 0}
+    {name: 'Test', type_of_marker: 4, x: 0, y: 0}
   }
 
   let(:user) { create :user }
@@ -26,6 +24,57 @@ RSpec.describe Api::Videos::MarkersController, :type => :controller do
       marker = create :video_marker, video: video
       get :show, {video_id: video.to_param, id: marker.to_param}
       expect(assigns(:marker)).to eq(marker)
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "creates a new Video::Marker" do
+        expect {
+          post :create, {video_id: video.to_param, :video_marker => valid_attributes}
+        }.to change(Video::Marker, :count).by(1)
+      end
+
+      it "assigns a newly created marker as @marker" do
+        post :create, {video_id: video.to_param, :video_marker => valid_attributes}
+        expect(assigns(:marker)).to be_a(Video::Marker)
+        expect(assigns(:marker)).to be_persisted
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved marker as @marker" do
+        post :create, {video_id: video.to_param, :video_marker => invalid_attributes}
+        expect(assigns(:marker)).to be_a_new(Video::Marker)
+      end
+    end
+  end
+
+  describe "PUT update" do
+    describe "with valid params" do
+      let(:new_attributes) { attributes_for(:video_marker) }
+
+      it "updates the requested marker" do
+        marker = create :video_marker, video: video
+        put :update, {video_id: video.to_param, :id => marker.to_param, :video_marker => new_attributes}
+        marker.reload
+
+        expect(marker.name).to eq(new_attributes[:name])
+      end
+
+      it "assigns the requested marker as @marker" do
+        marker = create :video_marker, video: video
+        put :update, {video_id: video.to_param, :id => marker.to_param, :video_marker => valid_attributes}
+        expect(assigns(:marker)).to eq(marker)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the marker as @marker" do
+        marker = create :video_marker, video: video
+        put :update, {video_id: video.to_param, :id => marker.to_param, :video_marker => invalid_attributes}
+        expect(assigns(:marker)).to eq(marker)
+      end
     end
   end
 
