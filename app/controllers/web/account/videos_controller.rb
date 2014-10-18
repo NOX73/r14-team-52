e7e4d681eae10859490fd24bb03c128a75801bc4
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-10-19 01:51:40 +0400

    show to user only his/her videos

--- app/controllers/web/account/videos_controller.rb
+++ app/controllers/web/account/videos_controller.rb
@@ -2,7 +2,7 @@ class Web::Account::VideosController < Web::Account::ApplicationController
 
   # GET /videos
   def index
-    @videos = Video.all
+    @videos = current_user.videos.all
   end
 
   # GET /videos/1
@@ -12,17 +12,17 @@ class Web::Account::VideosController < Web::Account::ApplicationController
 
   # GET /videos/new
   def new
-    @video = Video.new
+    @video = current_user.videos.build
   end
 
   # GET /videos/1/edit
   def edit
-    @video = Video.find(params[:id])
+    @video = current_user.videos.find(params[:id])
   end
 
   # POST /videos
   def create
-    @video = Video.new(video_params)
+    @video = current_user.videos.build(video_params)
     @video.user = current_user
 
     if @video.save
@@ -34,7 +34,7 @@ class Web::Account::VideosController < Web::Account::ApplicationController
 
   # PATCH/PUT /videos/1
   def update
-    @video = Video.find(params[:id])
+    @video = current_user.videos.find(params[:id])
     if @video.update(video_params)
       redirect_to video_url(@video), notice: 'Video was successfully updated.'
     else
@@ -44,7 +44,7 @@ class Web::Account::VideosController < Web::Account::ApplicationController
 
   # DELETE /videos/1
   def destroy
-    @video = Video.find(params[:id])
+    @video = current_user.videos.find(params[:id])
     @video.destroy
     redirect_to videos_url, notice: 'Video was successfully destroyed.'
   end
--- spec/controllers/web/account/videos_controller_spec.rb
+++ spec/controllers/web/account/videos_controller_spec.rb
@@ -2,52 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Web::Account::VideosController, :type => :controller do
 
-  let(:user) {
-    attributes = {email: 'test1@test.com', password: '123', password_confirmation: '123'}
-    User.create! attributes
-  }
+  let(:user) { create :user }
+  let(:valid_attributes) { attributes_for :video }
+  let(:invalid_attributes) { {name: 'Test', source_url: ''} }
 
   before do
     login_user(user)
   end
 
-  let(:valid_attributes) {
-    {name: 'Test', source_url: 'https://www.youtube.com/watch?v=n_yHZ_vKjno' }
-  }
-
-  let(:invalid_attributes) {
-    {name: 'Test', source_url: ''}
-  }
-
-  let(:valid_session) { {} }
-
   describe "GET index" do
-    it "assigns all videos as @videos" do
-      video = Video.create! valid_attributes
-      get :index, {}, valid_session
+    it "assigns current user videos as @videos" do
+      video = create :video, user: user
+      get :index, {}
       expect(assigns(:videos)).to eq([video])
     end
   end
 
   describe "GET show" do
     it "assigns the requested video as @video" do
-      video = Video.create! valid_attributes
-      get :show, {:id => video.to_param}, valid_session
+      video = create :video
+      get :show, {:id => video.to_param}
       expect(assigns(:video)).to eq(video)
     end
   end
 
   describe "GET new" do
     it "assigns a new video as @video" do
-      get :new, {}, valid_session
+      get :new, {}
       expect(assigns(:video)).to be_a_new(Video)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested video as @video" do
-      video = Video.create! valid_attributes
-      get :edit, {:id => video.to_param}, valid_session
+      video = create :video, user: user
+      get :edit, {:id => video.to_param}
       expect(assigns(:video)).to eq(video)
     end
   end
@@ -56,30 +45,30 @@ RSpec.describe Web::Account::VideosController, :type => :controller do
     describe "with valid params" do
       it "creates a new Video" do
         expect {
-          post :create, {:video => valid_attributes}, valid_session
+          post :create, {:video => valid_attributes}
         }.to change(Video, :count).by(1)
       end
 
       it "assigns a newly created video as @video" do
-        post :create, {:video => valid_attributes}, valid_session
+        post :create, {:video => valid_attributes}
         expect(assigns(:video)).to be_a(Video)
         expect(assigns(:video)).to be_persisted
       end
 
       it "redirects to the created video" do
-        post :create, {:video => valid_attributes}, valid_session
+        post :create, {:video => valid_attributes}
         expect(response).to redirect_to(video_url(Video.last))
       end
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved video as @video" do
-        post :create, {:video => invalid_attributes}, valid_session
+        post :create, {:video => invalid_attributes}
         expect(assigns(:video)).to be_a_new(Video)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {:video => invalid_attributes}, valid_session
+        post :create, {:video => invalid_attributes}
         expect(response).to render_template("new")
       end
     end
@@ -92,36 +81,36 @@ RSpec.describe Web::Account::VideosController, :type => :controller do
       }
 
       it "updates the requested video" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => new_attributes}, valid_session
+        video = create :video, user: user
+        put :update, {:id => video.to_param, :video => new_attributes}
         video.reload
 
         expect(video.name).to eq('Test')
       end
 
       it "assigns the requested video as @video" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => valid_attributes}, valid_session
+        video = create :video, user: user
+        put :update, {:id => video.to_param, :video => valid_attributes}
         expect(assigns(:video)).to eq(video)
       end
 
       it "redirects to the video" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => valid_attributes}, valid_session
+        video = create :video, user: user
+        put :update, {:id => video.to_param, :video => valid_attributes}
         expect(response).to redirect_to(video_url(video))
       end
     end
 
     describe "with invalid params" do
       it "assigns the video as @video" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => invalid_attributes}, valid_session
+        video = create :video, user: user
+        put :update, {:id => video.to_param, :video => invalid_attributes}
         expect(assigns(:video)).to eq(video)
       end
 
       it "re-renders the 'edit' template" do
-        video = Video.create! valid_attributes
-        put :update, {:id => video.to_param, :video => invalid_attributes}, valid_session
+        video = create :video, user: user
+        put :update, {:id => video.to_param, :video => invalid_attributes}
         expect(response).to render_template("edit")
       end
     end
@@ -129,15 +118,15 @@ RSpec.describe Web::Account::VideosController, :type => :controller do
 
   describe "DELETE destroy" do
     it "destroys the requested video" do
-      video = Video.create! valid_attributes
+      video = create :video, user: user
       expect {
-        delete :destroy, {:id => video.to_param}, valid_session
+        delete :destroy, {:id => video.to_param}
       }.to change(Video, :count).by(-1)
     end
 
     it "redirects to the videos list" do
-      video = Video.create! valid_attributes
-      delete :destroy, {:id => video.to_param}, valid_session
+      video = create :video, user: user
+      delete :destroy, {:id => video.to_param}
       expect(response).to redirect_to(videos_url)
     end
   end
