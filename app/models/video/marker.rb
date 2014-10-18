1bc4d32fda3681d43dae806f5f16e505c41a89a2
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-10-18 23:06:42 +0400

    remove name validation

--- app/models/video/marker.rb
+++ app/models/video/marker.rb
@@ -3,7 +3,6 @@ class Video::Marker < ActiveRecord::Base
 
   # enum type_of_marker: {info: 0, link: 1}
 
-  validates :name, presence: true
   validates :start_at, presence: true
   validates :x, presence: true
   validates :y, presence: true
