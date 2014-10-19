f618c003903134493b631e256948f19b1429c16f
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-10-19 22:44:08 +0400

    s3

--- app/uploaders/video/marker_image_uploader.rb
+++ app/uploaders/video/marker_image_uploader.rb
@@ -2,14 +2,6 @@
 
 class Video::MarkerImageUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -22,8 +14,8 @@ class Video::MarkerImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :scale => [32, 32]
+
   #
   # def scale(width, height)
   #   # do something
