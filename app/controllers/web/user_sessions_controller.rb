1ef7b52a7066a400059cf32b6cf6e2888c19d42c
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-10-19 13:44:09 +0400

    fix login form :fire:

--- app/controllers/web/user_sessions_controller.rb
+++ app/controllers/web/user_sessions_controller.rb
@@ -6,10 +6,11 @@ class Web::UserSessionsController < ApplicationController
   end
 
   def create
-    @user = User.new
     if @user = login(user_params[:email], user_params[:password])
       redirect_back_or_to(:videos, notice: 'Login successful')
     else
+      @user = User.new(email: user_params[:email])
+      @user.errors[:email] << 'Oops.. Check your login and password'
       flash.now[:alert] = 'Login failed'
       render action: 'new'
     end
