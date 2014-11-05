1f956e600413bc90ab7f86d3218858f0c51c84e4
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-11-05 17:11:48 +0300

    new unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,30 +1,40 @@
-# https://devcenter.heroku.com/articles/rails-unicorn
+project_home = ENV['UNICORN_HOME'] || '.'
+working_directory project_home
 
-worker_processes (ENV["WEB_CONCURRENCY"] || 3).to_i
-timeout (ENV["WEB_TIMEOUT"] || 5).to_i
+worker_processes (ENV['UNICORN_WORKERS'] || 1).to_i
+
+listen ENV['APP_ROOT'] + '/shared/.sock', backlog: 64
+timeout (ENV['UNICORN_TIMEOUT'] || 40).to_i
+
+pid ENV['APP_ROOT'] + '/shared/pids/unicorn.pid'
+
+# combine REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
 
-before_fork do |server, worker|
-  Signal.trap "TERM" do
-    puts "Unicorn master intercepting TERM and sending myself QUIT instead"
-    Process.kill "QUIT", Process.pid
-  end
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
 
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
   if defined? ActiveRecord::Base
     ActiveRecord::Base.connection.disconnect!
+  end
+
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
   end
 end
 
 after_fork do |server, worker|
-  Signal.trap "TERM" do
-    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
   end
 
-  if defined? ActiveRecord::Base
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-      Rails.application.config.database_configuration[Rails.env]
-    config["reaping_frequency"] = (ENV["DB_REAPING_FREQUENCY"] || 10).to_i
-    config["pool"] = (ENV["DB_POOL"] || 2).to_i
-    ActiveRecord::Base.establish_connection(config)
+  if defined?(Resque)
+    Resque.redis = ENV['REDIS_URI']
+    Rails.logger.info('Connected to Redis')
   end
 end
